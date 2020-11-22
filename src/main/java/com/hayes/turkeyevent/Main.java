a15ee9fcbf24e2624c9cf3972db2e1e2ba92e617
BuildTools <unconfigured@null.spigotmc.org>
2020-11-21 23:42:55 -0600

    update

--- src/main/java/com/hayes/turkeyevent/Main.java
+++ src/main/java/com/hayes/turkeyevent/Main.java
@@ -3,25 +3,38 @@ package com.hayes.turkeyevent;
 import com.hayes.turkeyevent.commands.CurrentEggs;
 import com.hayes.turkeyevent.commands.HighestEggs;
 import com.hayes.turkeyevent.commands.ReLore;
+import com.hayes.turkeyevent.mobs.Turkey;
 import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Chicken;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
+import org.bukkit.loot.Lootable;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.bukkit.Sound.ENTITY_CHICKEN_EGG;
 
@@ -29,6 +42,7 @@ public final class Main extends JavaPlugin implements Listener {
 
     private File customUuidFile;
     private FileConfiguration uuidFile;
+    private int playerTask;
 
     public static Main plugin;
 
@@ -41,12 +55,6 @@ public final class Main extends JavaPlugin implements Listener {
         this.getCommand("relore").setExecutor(new ReLore());
         this.getCommand("currenteggs").setExecutor(new CurrentEggs());
         this.getCommand("highesteggs").setExecutor(new HighestEggs());
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        }, (int) (Math.random() * 12000) + 12000, 12000L);
         // Output to Console
         System.out.println("TURKEY EVENT - Plugin has loaded");
     }
@@ -92,11 +100,24 @@ public final class Main extends JavaPlugin implements Listener {
             uuidFile.createSection(playerUUID + ".eggs");
             uuidFile.set(playerUUID + ".eggs", 0);
         }
+
+        playerTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Turkey.spawnTurkey(player);
+            }
+        }.runTaskTimer(this, 1L, (int) (Math.random() * 12000) + 12000).getTaskId();
+
         try {
             uuidFile.save(customUuidFile);
         } catch (IOException exception1) {
             exception1.printStackTrace();
         }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        Bukkit.getScheduler().cancelTask(playerTask);
     }
 
 
@@ -106,28 +127,82 @@ public final class Main extends JavaPlugin implements Listener {
             Player player = (Player) e.getEntity().getShooter();
             ItemStack egg = player.getInventory().getItemInMainHand();
             List<String> lore = player.getInventory().getItemInMainHand().getItemMeta().getLore();
-            if (lore != null && lore.get(0).equals(ChatColor.GREEN + "Holiday Item")) {
+            if (lore != null && lore.get(1).equals(ChatColor.GREEN + "Thanksgiving Item")) {
 
-                int randNum = (int) (Math.random() * 5) + 1;
+                double randNum = (Math.random() * 5);
                 ItemStack randItem;
                 e.setCancelled(true);
 
-                switch (randNum) {
-                    case 1:
-                        randItem = new ItemStack(Material.DIAMOND);
-                        break;
-                    case 2:
-                        randItem = new ItemStack(Material.EMERALD);
-                        break;
-                    case 3:
-                        randItem = new ItemStack(Material.IRON_INGOT);
-                        break;
-                    case 4:
-                        randItem = new ItemStack(Material.GOLD_INGOT);
-                        break;
-                    default:
-                        randItem = new ItemStack(Material.APPLE);
+                if (randNum >= 0 && randNum <= 0.25) {
+                    randItem = new ItemStack(Material.EMERALD, 4);
                 }
+                else if (randNum >= 0.26 && randNum <= 0.50) {
+                    randItem = new ItemStack(Material.LAPIS_LAZULI, 16);
+                }
+                else if (randNum >= 0.51 && randNum <= 0.75) {
+                    randItem = new ItemStack(Material.REDSTONE, 16);
+                }
+                else if (randNum >= 0.76 && randNum <= 1.00) {
+                    randItem = new ItemStack(Material.DIAMOND);
+                }
+                else if (randNum >= 1.01 && randNum <= 1.25) {
+                    randItem = new ItemStack(Material.IRON_INGOT, 4);
+                }
+                else if (randNum >= 1.26 && randNum <= 1.50) {
+                    randItem = new ItemStack(Material.GOLD_INGOT, 2);
+                }
+                else if (randNum >= 1.51 && randNum <= 1.75) {
+                    randItem = new ItemStack(Material.DIAMOND);
+                }
+                else if (randNum >= 1.76 && randNum <= 1.77) {
+                    randItem = new ItemStack(Material.NETHERITE_SCRAP);
+                }
+                else if (randNum >= 1.78 && randNum <= 2.00) {
+                    randItem = new ItemStack(Material.COAL_BLOCK);
+                }
+                else if (randNum >= 2.01 && randNum <= 2.25) {
+                    randItem = new ItemStack(Material.FIREWORK_ROCKET, 16);
+                }
+                else if (randNum >= 2.26 && randNum <= 2.27) {
+                    randItem = new ItemStack(Material.WITHER_SKELETON_SKULL);
+                }
+                else if (randNum >= 2.28 && randNum <= 2.50) {
+                    randItem = new ItemStack(Material.CAKE);
+                }
+                else if (randNum >= 2.51 && randNum <= 2.75) {
+                    randItem = new ItemStack(Material.GOLDEN_APPLE);
+                }
+                else if (randNum >= 2.76 && randNum <= 3.00) {
+                    randItem = new ItemStack(Material.COAL_BLOCK);
+                }
+                else if (randNum >= 3.01 && randNum <= 3.25) {
+                    randItem = new ItemStack(Material.LARGE_FERN, 4);
+                }
+                else if (randNum >= 3.26 && randNum <= 3.50) {
+                    randItem = new ItemStack(Material.PUFFERFISH);
+                }
+                else if (randNum >= 3.51 && randNum <= 3.75) {
+                    randItem = new ItemStack(Material.RABBIT_FOOT);
+                }
+                else if (randNum >= 3.76 && randNum <= 4.00) {
+                    randItem = new ItemStack(Material.SADDLE);
+                }
+                else if (randNum >= 4.01 && randNum <= 4.25) {
+                    randItem = new ItemStack(Material.NAME_TAG);
+                }
+                else if (randNum >= 4.26 && randNum <= 4.50) {
+                    randItem = new ItemStack(Material.LEAD);
+                }
+                else if (randNum >= 4.51 && randNum <= 4.75) {
+                    randItem = new ItemStack(Material.FLINT, 8);
+                }
+                else if (randNum >= 4.76 && randNum <= 5.00) {
+                    randItem = new ItemStack(Material.COOKIE, 4);
+                }
+                else {
+                    randItem = new ItemStack(Material.APPLE);
+                }
+
 
                 player.getWorld().dropItem(player.getLocation(), randItem);
                 player.playSound(player.getLocation(), ENTITY_CHICKEN_EGG, 1, 0);
@@ -166,6 +241,62 @@ public final class Main extends JavaPlugin implements Listener {
                     } catch (IOException exception1) {
                         exception1.printStackTrace();
                     }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e) {
+        if (e.getEntity().getKiller() instanceof Player) {
+            if (e.getEntity() instanceof Chicken && e.getEntity().getCustomName().equals(ChatColor.GOLD + "Turkey")) {
+                Player player = e.getEntity().getKiller();
+
+                e.getDrops().clear();
+
+                ItemStack item = new ItemStack(Material.EGG);
+                List<String> lore = new ArrayList<>();
+                lore.add(" ");
+                lore.add(ChatColor.GREEN + "Thanksgiving Item");
+                ItemMeta itemMeta = item.getItemMeta();
+                itemMeta.setLore(lore);
+                itemMeta.setDisplayName(ChatColor.GOLD + "Turkey" + ChatColor.WHITE + " Egg");
+                item.setItemMeta(itemMeta);
+
+                LootContext.Builder contextBuilder = new LootContext.Builder(e.getEntity().getLocation()).lootedEntity(e.getEntity()).killer(player);
+                AttributeInstance attributeLuck = player.getAttribute(Attribute.GENERIC_LUCK);
+
+                if (attributeLuck != null) {
+                    contextBuilder.luck(((float) attributeLuck.getValue()));
+                }
+
+                contextBuilder.lootingModifier(player.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS));
+
+                LootContext context = contextBuilder.build();
+
+                LootTable lootile = new LootTable() {
+                    @Override
+                    public Collection<ItemStack> populateLoot(Random random, LootContext context) {
+                        Collection<ItemStack> itemStack = new ArrayList<>();
+                        for (int i = -1; i < context.getLootingModifier() * (int) (Math.random() * 2); i++) {
+                            itemStack.add(item);
+                        }
+                        return itemStack;
+                    }
+
+                    @Override
+                    public void fillInventory(Inventory inventory, Random random, LootContext context) {
+
+                    }
+
+                    @Override
+                    public NamespacedKey getKey() {
+                        return null;
+                    }
+                };
+
+                for (ItemStack loot : lootile.populateLoot(ThreadLocalRandom.current(), context)) {
+                    e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(), loot);
                 }
             }
         }
